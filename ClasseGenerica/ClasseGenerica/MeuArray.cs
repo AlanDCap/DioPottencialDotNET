ff3c85c765474fe3c8387f1ae45833d998d6fee7
Alan Capelari <alandc@hotmail.com.br>
2022-10-24 11:27:28 -0300

    Envio da aula de Classes Genéricas.

---
+++ ClasseGenerica/ClasseGenerica/MeuArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseGenerica
+{
+    internal class MeuArray<T>
+    {
+        private static int capacidade = 10;
+        private int contador = 0;
+        private T[] arr = new T[capacidade];
+
+        public void AdicionarElementoArray(T elemento)
+        {
+            if (contador +1 < 11)
+            {
+                arr[contador] = elemento;
+            }
+            contador++;
+        }
+        
+        public T this[int index]
+        {
+            get { return arr[index]; }
+            set { arr[index] = value; }
+        }
+    }
+}
---
+++ ClasseGenerica/ClasseGenerica/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClasseGenerica;
+
+int qtdAhoys;
+
+MeuArray<int> arrayDeInteiros = new MeuArray<int>();
+MeuArray<string> arrayDeStrings = new MeuArray<string>();
+
+Console.WriteLine("Quantos ahoys deseja?");
+qtdAhoys = int.Parse(Console.ReadLine());
+
+arrayDeInteiros.AdicionarElementoArray(qtdAhoys);
+arrayDeStrings.AdicionarElementoArray("Ahoy");
+
+Console.WriteLine($"{arrayDeInteiros[0]}" + $" {(arrayDeInteiros[0] > 1 ? arrayDeStrings[0] + "s" : arrayDeStrings[0] )} para ti");
