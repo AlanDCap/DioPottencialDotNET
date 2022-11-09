1d900ef41606037c09be7e193643ed0b746de6e2
Alan Capelari <alandc@hotmail.com.br>
2022-11-09 12:39:01 -0300

    Envio de exemplo de WebAPI feito no módulo de APIs.

---
+++ ModuloAPI/ModuloAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString(),
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo!";
+            return Ok(mensagem);
+        }
+
+    }
+}
